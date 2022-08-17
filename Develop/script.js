7e88b8e8ec3584f4395c7ece6f869fde7b66b386
shanicesauce <shanicesauceda@gmail.com>
2022-08-17 00:40:34 -0500

    time successfully adding colors based on past present future

--- Develop/script.js
+++ Develop/script.js
@@ -7,19 +7,47 @@ currentDay.text(moment().format("llll"));
 
 hours.each(function() {
     var plannerTime = parseInt($(this).attr("id"));
+    var siblings = $(this).siblings();
+   
     if (plannerTime < currentTime) {
+        siblings.addClass("past");
         console.log("past");
-        // description.addClass("past");
         //addclass past
     }
     else if (plannerTime > currentTime) {
+        siblings.addClass("future");
         console.log("future");
-        // description.addClass("future");
         //addclass future
     }
     else {
+        siblings.addClass("present");
         console.log("present");
-        // description.addClass("present");
         //addclass present
     }
-})
+    console.log(plannerTime)
+});
+
+
+$(".saveBtn").click(function() {
+    // save
+    savePlans();
+});
+
+
+
+var savePlans = function() {
+    var time =  $(hours).attr("id");
+    var timeChosen = parseInt(time)
+    var plannedText = $(".description").val();
+
+    localStorage.setItem(timeChosen, plannedText);
+
+    console.log(timeChosen);
+
+}
+
+// var loadPlans = function() {
+//  localStorage.getItem();
+// }
+
+// console.log(loadPlans)
